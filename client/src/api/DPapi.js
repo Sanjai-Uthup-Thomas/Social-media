d43571b6b7ba77b4b7bdd2bb7bfa10b75b3a435d
sanjai uthup thomas <sanjaiuthupthomas@gmail.com>
2023-02-11 19:41:10 +0530

    dp change api

--- client/src/api/DPapi.js
+++ client/src/api/DPapi.js
@@ -1,5 +1,5 @@
 // const DPurl = "http://localhost:4000/DP"
-const DPurl = "https://sanjaiuthupthomas.in/DP"
+const DPurl = "https://sanjaiuthupthomas.in/api/DP"
 
 
 export default DPurl
--- client/src/api/imagesApi.js
+++ client/src/api/imagesApi.js
@@ -1,5 +1,5 @@
 // const postsImages = "http://localhost:4000/images"
-const postsImages = "https://sanjaiuthupthomas.in/images"
+const postsImages = "https://sanjaiuthupthomas.in/api/images"
 
 
 export default postsImages
